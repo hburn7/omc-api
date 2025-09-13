fa8bc23acdb561095b10b215b821e7fb83470e35
hburn7 <hburnett777@gmail.com>
2025-09-13 19:02:53 -0400

    Add tests flow and a few end to end tests

---
+++ src/test/e2e.test.ts
@@ -0,0 +1,67 @@
+import { describe, it, expect } from "vitest";
+import { fetchBeatmaps } from "../lib/client";
+import * as validator from "../lib/validator";
+import { ComplianceFailureReason, ComplianceStatus } from "../lib/dataTypes";
+
+describe("End-to-end tests", () => {
+  describe("Beatmap fetch and validation flow", () => {
+    it("should fetch and validate beatmap 4074595 as OK", async () => {
+      const beatmapId = 4074595;
+
+      const fetchResult = await fetchBeatmaps([beatmapId]);
+
+      expect(fetchResult.failures).toHaveLength(0);
+      expect(fetchResult.beatmaps).toHaveLength(1);
+      expect(fetchResult.beatmaps[0]?.id).toBe(beatmapId);
+
+      const validationResults = validator.validate(fetchResult.beatmaps);
+
+      expect(validationResults).toHaveLength(1);
+      expect(validationResults[0]?.complianceStatus).toBe(ComplianceStatus.OK);
+    });
+
+    it("should fetch and validate beatmap 4062794 as disallowed [MEGAREX]", async () => {
+      const beatmapId = 4062794;
+
+      const fetchResult = await fetchBeatmaps([beatmapId]);
+      const beatmapset = fetchResult.beatmaps[0]?.beatmapset;
+
+      expect(beatmapset).not.null;
+      expect(fetchResult.failures).toHaveLength(0);
+      expect(fetchResult.beatmaps).toHaveLength(1);
+      expect(fetchResult.beatmaps[0]?.id).toBe(beatmapId);
+
+      const validationResults = validator.validate(fetchResult.beatmaps);
+
+      expect(validationResults).toHaveLength(1);
+      expect(validationResults[0]?.complianceStatus).toBe(ComplianceStatus.DISALLOWED);
+      expect(validationResults[0]?.complianceFailureReason).toBe(ComplianceFailureReason.DISALLOWED_BY_RIGHTSHOLDER);
+
+      logTitleArtist(beatmapset!.title, beatmapset!.artist);
+    });
+
+    it("should fetch and validate beatmap 1416722 as disallowed [DMCA]", async () => {
+      const beatmapId = 1416722;
+
+      const fetchResult = await fetchBeatmaps([beatmapId]);
+      const beatmapset = fetchResult.beatmaps[0]?.beatmapset;
+
+      expect(beatmapset).not.null;
+      expect(fetchResult.failures).toHaveLength(0);
+      expect(fetchResult.beatmaps).toHaveLength(1);
+      expect(fetchResult.beatmaps[0]?.id).toBe(beatmapId);
+
+      const validationResults = validator.validate(fetchResult.beatmaps);
+
+      expect(validationResults).toHaveLength(1);
+      expect(validationResults[0]?.complianceStatus).toBe(ComplianceStatus.DISALLOWED);
+      expect(validationResults[0]?.complianceFailureReason).toBe(ComplianceFailureReason.DMCA);
+
+      logTitleArtist(beatmapset!.title, beatmapset!.artist);
+    });
+  });
+});
+
+function logTitleArtist(title: string, artist: string) {
+  console.log(`[${artist} - ${title}]`)
+}
