6afeb56db89e29ccf48233f3f93f93394cce9a5f
hburn7 <hburnett777@gmail.com>
2025-09-19 11:53:47 -0400

    refactor: reuse osu api client

--- src/lib/osuApi.ts
+++ src/lib/osuApi.ts
@@ -1,39 +1,42 @@
 import * as osu from "osu-api-v2-js";
-
 import dotenv from "dotenv";
+
 dotenv.config();
 
-class ApiWrapper {
-  private static instance: osu.API | null = null;
-  private static initPromise: Promise<osu.API> | null = null;
-
-  private static async initialize(): Promise<osu.API> {
-    if (this.instance) {
-      return this.instance;
-    }
-
-    if (this.initPromise) {
-      return this.initPromise;
-    }
-
-    this.initPromise = (async () => {
-      const clientId = Number(process.env.OSU_CLIENT_ID);
-      const clientSecret = process.env.OSU_CLIENT_SECRET;
-
-      if (!clientSecret) {
-        throw new Error("OSU_CLIENT_SECRET not configured");
-      }
-
-      this.instance = await osu.API.createAsync(clientId, clientSecret);
-      return this.instance;
-    })();
-
-    return this.initPromise;
-  }
-
-  static async getApi(): Promise<osu.API> {
-    return this.initialize();
-  }
+declare global {
+  // eslint-disable-next-line no-var
+  var __omc_osu_api_instance: Promise<osu.API> | undefined;
 }
 
-export const api = await ApiWrapper.getApi();
+const resolveCredentials = () => {
+  const rawClientId = process.env.OSU_CLIENT_ID;
+  const clientSecret = process.env.OSU_CLIENT_SECRET;
+
+  if (!rawClientId) {
+    throw new Error("OSU_CLIENT_ID not configured");
+  }
+  if (!clientSecret) {
+    throw new Error("OSU_CLIENT_SECRET not configured");
+  }
+
+  const clientId = Number.parseInt(rawClientId, 10);
+
+  if (Number.isNaN(clientId)) {
+    throw new Error("OSU_CLIENT_ID must be a valid number");
+  }
+
+  return { clientId, clientSecret };
+};
+
+const createApiInstance = async (): Promise<osu.API> => {
+  const { clientId, clientSecret } = resolveCredentials();
+  return osu.API.createAsync(clientId, clientSecret);
+};
+
+const apiPromise: Promise<osu.API> = globalThis.__omc_osu_api_instance ?? (
+  globalThis.__omc_osu_api_instance = createApiInstance()
+);
+
+export const getApi = (): Promise<osu.API> => apiPromise;
+
+export const api: osu.API = await apiPromise;
